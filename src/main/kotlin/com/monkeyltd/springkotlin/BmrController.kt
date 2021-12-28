bf35f7419bbfc8872318b82a215c9e2b003219e9
okerules <dmeyer5@live.com>
2021-12-28 14:08:53 -0500

    Removed tests for now. Will add back appropriate testing. Updated import.

--- src/main/kotlin/com/monkeyltd/springkotlin/BmrController.kt
+++ src/main/kotlin/com/monkeyltd/springkotlin/BmrController.kt
@@ -1,34 +1,30 @@
 package com.monkeyltd.springkotlin
 
-import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequelstParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class BmrController {
 
     @GetMapping("/bmr")
     fun getbmr(
-            @RequestParam(value = "age") age: Int?,
-            @RequestParam(value = "gender") gender: String?,
-            @RequestParam(value = "units") units: String?,
-            @RequestParam(value = "height") height: Int?,
-            @RequestParam(value = "weight") weight: Int?
+        @RequestParam(value = "age") age: Int?,
+        @RequestParam(value = "gender") gender: String?,
+        @RequestParam(value = "units") units: String?,
+        @RequestParam(value = "height") height: Int?,
+        @RequestParam(value = "weight") weight: Int?
     ): Results {
         if (age == null || gender == null || units == null || height == null || weight == null){
-            return Results("Values missing");
+            return Results("Values missing")
         }
-        var bmr: Int
-        var wtKg: Int
-        var htCm: Int
-        wtKg = if (units == "English") {
+        val bmr: Int
+
+        val wtKg: Int = if (units == "English") {
             (weight.toDouble() * 0.45359237).toInt()
         } else {
             weight
         }
 
-        htCm = if (units == "English"){
+        val htCm: Int = if (units == "English"){
             (height.toDouble() * 2.54).toInt()
         } else {
             height
